4f9e75558ff43817427bc618cf6b9d2c8f8848f4
ejller <ejller@bk.ru>
2020-01-05 03:56:19 +0500

    add redux store

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App/App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import {store } from './_helpers/store'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
 serviceWorker.unregister();
